53f3ef9db65ae4e6e654e266b26e7058e93b1c2a
kim22016-del <kim22016@byui.edu>
2026-01-10 20:02:05 -0700

    Update Program.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        // This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your percentage of grade. ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+
+        string score = "none";
+
+        if (number > 90 || number == 90)
+        {
+            score = "A";
+        }
+        else if (number > 80 || number == 80)
+        {
+            score = "B";
+        }
+        else if (number > 70 || number == 70)
+        {
+            score = "C";
+        }
+        else if (number > 60 || number == 60)
+        {
+            score = "D";
+        }
+        else
+        {
+            score = "F";
+        }
+        Console.WriteLine($"Grade: {score}");
+
+        if (number > 60 || number == 60)
+        {
+            Console.WriteLine("Congratulation! you passed the test!");
+        }
+        else
+        {
+            Console.WriteLine("sorry you failed the test.");
+        }
     }
 }

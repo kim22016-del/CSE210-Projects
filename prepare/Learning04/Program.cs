813a4affe59fce15a605a8436d9dba1e6d937d4a
kim22016-del <kim22016@byui.edu>
2026-02-19 22:54:37 -0700

    Inheritance Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment newAssignment = new Assignment("Samuel Benett", "Multiplication");
+        string answer = newAssignment.GetSummary();
+
+        MathAssignment newMathAssignment = new MathAssignment("Roberto Rodriguez","Fractions","Section 7.3","Problems 8-19");
+        string answer2 = newMathAssignment.GetHomeworkList();
+
+        WritingAssignment newWritingAssignment = new WritingAssignment("Mary Waters","European Hisory", "The Causes of World War II");
+        string answer3 = newWritingAssignment.GetWritingInformation();
+
+        Console.WriteLine(answer);
+        Console.WriteLine(newMathAssignment.GetSummary());
+        Console.WriteLine(answer2);
+        Console.WriteLine(newWritingAssignment.GetSummary());
+        Console.WriteLine(answer3);
     }
 }
